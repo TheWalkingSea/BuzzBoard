3f6e109a08d47d3612c9ef8fadf72c5d7e549093
Austin McCallister <austinmccallister0@gmail.com>
2025-09-13 17:53:12 -0400

    Added time dependency to Clock

--- src/Components/Clock.tsx
+++ src/Components/Clock.tsx
@@ -10,7 +10,7 @@ export default function Clock() {
                 setTime(new Date());
             }, 1000);
             return () => clearTimeout(timeout);
-        }
+        }, [time]
     );
 
     function padTime(time: number): string {
