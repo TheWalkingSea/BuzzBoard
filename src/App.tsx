a8f11491a606e7743740805096ae7c270aca61cd
Austin McCallister <austinmccallister0@gmail.com>
2025-09-18 19:35:16 -0400

    Quote of the day implementation
    Need to actually connect to server

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       
       </header>
 
-      <QuoteOfTheDay quote="There is no passion to be found playing small - in settling for a life that is less than the one you are capable of living.\n- Nelson Mandela"/>
+      <QuoteOfTheDay />
       <Clock />
 
       <div style={{gridColumn: '1 / 6'}}>
--- src/Components/QuoteOfTheDay.tsx
+++ src/Components/QuoteOfTheDay.tsx
@@ -1,10 +1,34 @@
 import './QuoteOfTheDay.css'
+import { get_ms_until_midnight } from '../utils/Dates.ts';
+import { useState, useEffect } from 'react';
 
-type QuoteProp = {
-    quote: string
-};
 
-export default function QuoteOfTheDay({ quote }: QuoteProp) {
+
+function getQuote(): Promise<string> {
+    return fetch('https://zenquotes.io/api/today')
+    .then((response) => response.json())
+    .then((response) => `${response[0].q} \n ${response[0].a}`)
+}
+
+export default function QuoteOfTheDay() {
+    const [quote, setQuote] = useState("Loading quote...");
+
+    getQuote().then((q) => console.log(q));
+
+    useEffect(() => {
+        getQuote().then(q => setQuote(q));
+    }, []);
+
+    useEffect(() => {
+        const ms_until_midnight: number = get_ms_until_midnight();
+
+        const timeout = setTimeout(() => {
+            getQuote().then(q => setQuote(q));
+        }, ms_until_midnight);
+        
+        return () => clearTimeout(timeout);
+    }, [quote])
+
     return (
         <section className="quote">
             <div className="containerHeading">
